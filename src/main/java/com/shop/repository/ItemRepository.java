a658b24f0ac3c3278b7220e0944c18851181d1e5
networkSorcerer <gigattfyhig@gmail.com>
2024-02-19 20:18:13 +0900

    shop

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package com.shop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.shop.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
